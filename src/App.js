2123748669261704dae3e7bb15170c2d8164d052
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-01 16:18:19 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Quote from './components/Quote';
 
 const initialQuote = {
@@ -8,18 +8,40 @@ const initialQuote = {
 
 function App() {
 
-  const [quote, setQuote] = useState(initialQuote);
+  const [quote, setQuote] = useState(initialQuote)
+  const [loading, setLoading] = useState(false)
 
+  const updateQuote = async () => {
+    setLoading(true)
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    const res = await fetch(url)
+    const [newQuote] = await res.json();
+
+    const { quote: text, author } = newQuote;
+
+    setQuote({
+      text,
+      author
+    })
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    updateQuote();
+  }, [])
 
 
   return (
-    <div className="App">
+    <div className="app">
       <img
         src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
         alt="logo"
       />
-      <button>Another Quote</button>
-      <Quote quote={quote} />
+      <button onClick={() => updateQuote()}>Another Quote</button>
+
+      {loading ? <h2 class="loading" >Loading...</h2> : <Quote quote={quote} />}
+
     </div>
   );
 }
