8d9765b1884479b4b626a02c40178d9ddbb5b348
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-03-01 14:19:08 -0300

    Styles added and created first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useState } from 'react';
+import Quote from './components/Quote';
 
+const initialQuote = {
+  text: '',
+  author: ''
+}
 
 function App() {
+
+  const [quote, setQuote] = useState(initialQuote);
+
+
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button>Another Quote</button>
+      <Quote quote={quote} />
     </div>
   );
 }
